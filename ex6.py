1ed70f3da44f64b95e873ae0c13117882e9223ae
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2018-10-10 14:53:15 +0100

    Task 6
    Task 6 showing how python uses inputs

---
+++ ex6.py
@@ -0,0 +1,20 @@
+Name = input ("What is your name?")
+Age = input ("How old are you?")
+Height = ("How tall are you in Inches?")
+Weight = ("What is your weight in Kilograms?")
+Eyes = ("What colour are your eyes?")
+Hair = ("What colour hair do you have")
+
+print (Name)
+if Age > 60:
+    print ("You are over 60, relax and enjoy")
+if Age < 21:
+    print ("alot of life left to go")
+if Height > 70:
+    print ("You are taller than the average male")
+if Height < 66:
+    print ("You are shorter than the average person")
+if Weight > 80:
+    print ("You are over the average male weight")
+if weight < 80:
+    print ("You are bellow the average male weight")
