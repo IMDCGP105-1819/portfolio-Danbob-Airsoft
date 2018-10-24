455efaa841882f2a7678ab976b77865f2fe823ce
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2018-10-24 18:14:46 +0100

    Ex10
    Example 10 with input (Previous comit was missing input)

--- ex10.py
+++ ex10.py
@@ -1,5 +1,6 @@
-Number = 1
-for Number in range(1,100):
+Numberlow = int(input("Please input the low end of your range "))
+Numberhigh = int(input("Please input the high end of your range "))
+for Number in range(Numberlow,Numberhigh + 1):
     if Number % 3 == 0 and Number % 5 == 0:
         print ("FizzBuzz")
     elif Number % 3 ==0:
